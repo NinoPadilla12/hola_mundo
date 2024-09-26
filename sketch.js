7424d4a1caaba263ee9c50a622678f55cfa853a6
Nino Padilla <nino.padilla@ucuenca.edu.ec>
2024-09-26 16:38:48 -0500

    Modificación del Cursos

--- sketch.js
+++ sketch.js
@@ -6,9 +6,22 @@ function setup() {
 }
 
 function draw() {
-  background(250, random(0, 20), 0);
-  fill(0, mouseY, 255);
-  circle(mouseX + 80, mouseY - 80, mouseX);
-  fill(0, 250, 0);
-  rect(mouseX, mouseY, 100, 100);
+  background(100, random(100, 200), 100, 10);
+
+  stroke(random(0, 255), random(0, 255), random(0, 255));
+  strokeWeight(random(0, 20));
+  noFill(0);
+  rect(mouseX, mouseY, 200, mouseY, 0, mouseY, 0, mouseX);
+
+  // Style the Pacman arc.
+  noStroke();
+  fill(255, 255, random(0, 255));
+
+  // Update start and stop angles.
+  let biteSize = PI / 16;
+  let startAngle = biteSize * sin(frameCount * 0.1) + biteSize;
+  let endAngle = TWO_PI - startAngle;
+
+  // Draw the arc.
+  arc(mouseX, mouseY, 80, 80, startAngle, endAngle, PIE);
 }
